b6c9d920e7e48a32fa6159ec3b20362e397df9ed
jungwoohong <jungwoo02@gmail.com>
2022-09-27 15:47:46 +0900

    레이아웃 변경

--- newjw/newjw/board/models.py
+++ newjw/newjw/board/models.py
@@ -1,11 +1,42 @@
 from django.db import models
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+# 게시판 카테고리
+class categories(models.Model):
+    category_type = models.CharField(max_length=50)
+    category_code = models.CharField(max_length=100)
+    category_name = models.CharField(max_length=100)
+    category_desc = models.CharField(max_length=100)
+    list_count    = models.IntegerField(blank=True, null=True)
+    authority     = models.IntegerField(blank=True, null=True)
+    create_date   = models.DateTimeField(auto_now_add=True)
+    last_update_date = models.DateTimeField(auto_now=True)
 
+# 게시물
+class post(models.Model):
+    category = models.ForeignKey(categories, on_delete=models.CASCADE)
+    user_id  = models.CharField(max_length=100)
+    title    = models.CharField(max_length=300)
+    conent   = models.TextField()
+    registered_date = models.DateTimeField(auto_now_add=True)
+    last_update_date= models.DateTimeField(auto_now=True)
+    view_count = models.IntegerField(blank=True, default=0)
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+#댓글
+class replies(models.Model):
+    article   = models.ForeignKey(post, on_delete=models.CASCADE)
+    user_id   = models.CharField(max_length=100)
+    level     = models.IntegerField(blank=True, null=True)
+    conent   = models.TextField()
+    registered_date = models.DateTimeField(auto_now_add=True)
+    last_update_date= models.DateTimeField(auto_now=True)
+
+#첨부파일
+class upload_file(models.Model):
+    upload   = models.ForeignKey(post, on_delete=models.CASCADE)
+    file_path   = models.CharField(max_length=100)
+    file_name   = models.CharField(max_length=100)
+    file_real_name   = models.CharField(max_length=100)
+    file_ext   = models.CharField(max_length=100)
+    file_size   = models.CharField(max_length=100)
+    registered_date = models.DateTimeField(auto_now_add=True)
+    last_update_date= models.DateTimeField(auto_now=True)       
--- newjw/newjw/board/views.py
+++ newjw/newjw/board/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Question
+
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'board/index.html',context)
+    return render(request, 'board/index.html')
