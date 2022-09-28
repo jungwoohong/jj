4350027c3b5716cff645d8cdfe0bbde40baa234b
jungwoo <jungwoo02@gmail.com>
2022-09-28 19:06:41 +0900

    변경

--- newjw/newjw/board/views.py
+++ newjw/newjw/board/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.contrib.auth.decorators import login_required
+from .models import post
 
 @login_required
 def index(request):
-    post_data = Board.object.all();
-    return render(request, 'board/boardList.html',post_data)
+    posts = post.objects.all()
+    return render(request, 'board/boardList.html',{'posts':posts})
