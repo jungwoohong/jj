52744702abd3540b1235626f34efb11326ac10b4
jungwoo <jungwoo02@gmail.com>
2022-09-28 21:17:38 +0900

    ajax로 데이터 표출

--- newjw/newjw/board/urls.py
+++ newjw/newjw/board/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from . import views
+from newjw.board.views import *
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index.as_view()),
+    path('getBoardList',getBoardList.as_view()),
 ]
--- newjw/newjw/board/views.py
+++ newjw/newjw/board/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import post
+import json
 
-@login_required
-def index(request):
-    posts = post.objects.all()
-    return render(request, 'board/boardList.html',{'posts':posts})
+class index(LoginRequiredMixin, View):    
+    def get(self, request):
+        return render(request, 'board/boardList.html')
+
+class getBoardList(LoginRequiredMixin, View):
+
+    def get(self, request):
+        posts = post.objects.all()
+        post_list = []
+        for a in posts:
+            post_list.append({
+                'id':a.id,
+                'title':a.title,
+                'user_id':a.user_id,
+                'registered_date':a.registered_date,
+                'view_count':a.view_count
+            })
+        # postserial = serializers.serialize('json', posts)
+        # rs = {'data':json.loads(postserial)}
+        return JsonResponse({'data':post_list}, safe=False)
+          
