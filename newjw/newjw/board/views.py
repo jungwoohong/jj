65829cfb7a83d8b0db07e0c766d8c45487c66bc1
jungwoo <jungwoo02@gmail.com>
2022-09-29 23:50:33 +0900

    페이징(검색제외)

--- newjw/newjw/board/views.py
+++ newjw/newjw/board/views.py
@@ -1,29 +1,70 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.core import serializers
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.serializers.json import DjangoJSONEncoder
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import post
+from django.db.models import Q
 import json
 
-class index(LoginRequiredMixin, View):    
+from .models import post
+
+
+class index(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, 'board/boardList.html')
+
 
 class getBoardList(LoginRequiredMixin, View):
 
     def get(self, request):
+
+        datatables = request.GET
+        draw = int(datatables.get('draw'))
+        start = int(datatables.get('start'))
+        length = int(datatables.get('length'))
+        search = datatables.get('search[value]')
+        print("draw : ", draw, "start : ", start, "length : ", length)
         posts = post.objects.all()
-        post_list = []
-        for a in posts:
-            post_list.append({
-                'id':a.id,
-                'title':a.title,
-                'user_id':a.user_id,
-                'registered_date':a.registered_date,
-                'view_count':a.view_count
-            })
-        # postserial = serializers.serialize('json', posts)
-        # rs = {'data':json.loads(postserial)}
-        return JsonResponse({'data':post_list}, safe=False)
-          
+        posts_total = posts.count()
+        posts_filtered = posts_total
+        print("posts_total : " , posts_total)
+
+        if search:
+            posts = post.objects.filter(  # or 같은 조건문으로 사용할 때 Q()
+                Q(id__icontains=search) |
+                Q(title__icontains=search) |
+                Q(user_id__icontains=search)
+            )
+            posts_total = posts.count()
+            posts_filtered = posts_total
+
+        paginator = Paginator(posts, length)
+        page_number = int(start / length) + 1
+
+        try:
+            object_list = paginator.page(page_number).object_list
+        except PageNotAnInteger:
+            object_list = paginator.page(1).object_list
+        except EmptyPage:
+            object_list = paginator.page(1).object_list
+        
+        print(page_number)
+
+        data = [{
+                'id': ol.id,
+                'title': ol.title,
+                'user_id': ol.user_id,
+                'registered_date': ol.registered_date,
+                'view_count': ol.view_count
+			    } for ol in object_list
+		    ]
+
+        return JsonResponse({
+            'draw': draw,
+			'recordsTotal': posts_total,
+			'recordsFiltered': posts_filtered,
+            'data':data}, safe=False)
+
+
