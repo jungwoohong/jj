55c0d4fd636bb46556c30f4436aaefe6c96bd794
jungwoo <jungwoo02@gmail.com>
2022-10-05 23:34:31 +0900

    공지사항 등록

--- newjw/newjw/board/urls.py
+++ newjw/newjw/board/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', index.as_view()),
     path('getBoardListData/',getBoardListData.as_view(), name="getBoardListData"),
     path('postReg/',postReg.as_view(), name="postReg"),
+    path('postSave/',postSave.as_view(), name="postSave")
 ]
