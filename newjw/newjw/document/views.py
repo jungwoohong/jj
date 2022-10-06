e6f6757c4400acab2b1973654dd6b3af74bec3c5
jungwoohong <jungwoo02@gmail.com>
2022-10-06 19:56:25 +0900

    formError 체크

--- newjw/newjw/document/views.py
+++ newjw/newjw/document/views.py
@@ -5,7 +5,10 @@ from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404, JsonResponse
 import pandas as pd
-
+from django.utils import timezone
+from .forms import postForm, dataCollectionForm, shareUserForm
+from .models import post, data_collection, share_user
+from datetime import datetime
 
 class docReg(LoginRequiredMixin, View):
 
@@ -45,10 +48,38 @@ class docUpload(LoginRequiredMixin, View):
 
         return JsonResponse(retrunMsg)
 
+class docSave(LoginRequiredMixin, View):
 
-class autocompleteData(LoginRequiredMixin, View):
-    
-    def post(self, request):
-        data = ['홍길동','홍정우','홍천재']
-        retrunMsg = {"data": data}
-        return JsonResponse(retrunMsg)
+    def post(self, request, *args, **kwargs):
+
+        msg = "실패하였습니다."
+
+        id          = request.POST.get('id')
+        json_data   = request.POST.get('json_data')
+        title       = request.POST.get('title')
+        loginId     = request.user.username
+        start_date  = datetime.strptime(request.POST.get('start_date'), "%Y.%m.%d")
+        start_date  = timezone.make_aware(start_date)
+        end_date    = datetime.strptime(request.POST.get('end_date'), "%Y.%m.%d")
+        end_date    = timezone.make_aware(end_date)
+                    
+        arr = {"email": loginId,"json_data": json_data, "title": title,"start_date":start_date,"end_date": end_date}            
+        form = postForm(arr)
+
+        if form.is_valid():
+            if id == '':
+                # record = form.save()
+                
+
+                msg = "저장하였습니다."                    
+            else :
+                updateData = post.objects.get(id=id)
+                updateData.json_data    = json_data
+                updateData.title        = title
+                updateData.start_date   = start_date
+                updateData.end_date     = end_date
+                updateData.save()
+                msg = "수정하였습니다."                         
+
+        retrunMsg = {"msg": msg, "form":form.errors}
+        return JsonResponse(retrunMsg)        
