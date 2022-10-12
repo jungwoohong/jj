4047c84ccf8108393224d14d87906bac4baa7fe4
jungwoo <jungwoo02@gmail.com>
2022-10-12 22:30:26 +0900

    첨부파일 업로드

--- newjw/newjw/board/forms.py
+++ newjw/newjw/board/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import post
+from .models import *
 
 class postForm(forms.ModelForm):
     class Meta:
         model = post
+        fields = '__all__'    
+
+class uploadFileForm(forms.ModelForm):
+    class Meta:
+        model = upload_file
         fields = '__all__'    
