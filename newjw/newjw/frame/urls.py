994a1617a389f950af76ae817872a6f64fca4c32
jungwoohong <jungwoo02@gmail.com>
2022-10-24 16:36:37 +0900

    버그수정

--- newjw/newjw/frame/urls.py
+++ newjw/newjw/frame/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('frameJsonData/', frameJsonData.as_view(),name="frameJsonData" ),
     path('docToSearchList/', docToSearchList.as_view(),name="docToSearchList" ),
     path('docToSearchListData/', docToSearchListData.as_view(),name="docToSearchListData" ),
-    path('mailtest/', mailtest.as_view(),name="mailtest" ),
     
 ]
